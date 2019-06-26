2e8bad6e3fcaeee44eb249128eefc53cf06e1747
Anatarist <52218898+anatarist@users.noreply.github.com>
2019-06-26 15:14:54 -0600

    Tweak Windows build script

--- build-windows.sh
+++ build-windows.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-rm -rf dist/spacehaven-modloader
+VERSION=`python -c 'import version; print(version.version)'`
+
+rm -rf dist/spacehaven-modloader dist/spacehaven-modloader-$VERSION.windows
 pyinstaller spacehaven-modloader.py --noconsole
 
+mv dist/spacehaven-modloader dist/spacehaven-modloader-$VERSION.windows
+
+start dist
